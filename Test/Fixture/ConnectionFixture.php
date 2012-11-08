189b9e2fcbc3662635a4d34812e93bc59c5afafb
Joel Byrnes <joel@razorit.com>
2012-11-08 15:12:01 -0500

    Fixed unneccesarily required fields

--- Test/Fixture/ConnectionFixture.php
+++ Test/Fixture/ConnectionFixture.php
@@ -15,10 +15,10 @@ class ConnectionFixture extends CakeTestFixture {
 		'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
 		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'comment' => 'facebook, twitter, etc.', 'charset' => 'utf8'),
 		'value' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'comment' => 'serialized array of authentication responses', 'charset' => 'utf8'),
-		'creator_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-		'modifier_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'creator_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'modifier_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
