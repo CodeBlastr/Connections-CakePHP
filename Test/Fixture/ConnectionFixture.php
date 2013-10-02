273cdeb8e4a8ca091fb8171bd90e688d5377ad60
Doug Eaton <doug@buildrr.com>
2013-10-02 11:07:32 -0400

    Imported new Connection Schema
    Using connection schema instead of fixture so that teh fixture isnt beign called twice.

--- Test/Fixture/ConnectionFixture.php
+++ Test/Fixture/ConnectionFixture.php
@@ -6,22 +6,11 @@
 class ConnectionFixture extends CakeTestFixture {
 
 /**
- * Fields
+ * Import
  *
  * @var array
  */
-	public $fields = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
-		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'comment' => 'facebook, twitter, etc.', 'charset' => 'utf8'),
-		'value' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'comment' => 'serialized array of authentication responses', 'charset' => 'utf8'),
-		'creator_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
-		'modifier_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
-	);
+	public $import = array('config' => 'Connections.Connection');
 
 /**
  * Records
