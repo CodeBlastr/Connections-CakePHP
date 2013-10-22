9bcd3462914b4e0f509e5034f762e78a2cfec240
Joel Byrnes <joel@razorit.com>
2013-10-22 17:23:45 -0400

    code formatting

--- Controller/ConnectionPaypalController.php
+++ Controller/ConnectionPaypalController.php
@@ -1,7 +1,4 @@
-<?PHP
-/**
- * @author Joel Byrnes <joel@razorit.com>
- */
+<?php
 class ConnectionPaypalController extends ConnectionsAppController {
     public $uses = array('Connections.ConnectionPaypal');
     public $components = array('Cookie');
@@ -9,9 +6,9 @@ class ConnectionPaypalController extends ConnectionsAppController {
     public function requestPermissions() {
         //$this->ConnectionPaypal->requestPermissions('/tasks/tasks/my/');
         $success = $this->ConnectionPaypal->requestPermissions($this->referer());
-        if($success) {
+        if ($success) {
             $this->Cookie->write('ZuhaConnectionsPaypalToken', $success->token, false, 900);
-            if(ConnectionManager::getDataSource('paypal')->config['environment'] == 'sandbox') {
+            if (ConnectionManager::getDataSource('paypal')->config['environment'] == 'sandbox') {
                 $this->redirect('https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_grant-permission&request_token='.$success->token);
             } else {
                 $this->redirect('https://www.paypal.com/cgi-bin/webscr?cmd=_grant-permission&request_token='.$success->token);
@@ -20,9 +17,9 @@ class ConnectionPaypalController extends ConnectionsAppController {
     }
     
     public function createAccount() {
-        if($this->data) {
+        if ($this->data) {
             $success = $this->ConnectionPaypal->createAccount($this->data, $this->referer());
-            if($success) {
+            if ($success) {
                 $this->redirect($success->redirectUrl);
             } else {
                 $this->Session->setFlash('Please ensure your information is correct.');
@@ -35,37 +32,38 @@ class ConnectionPaypalController extends ConnectionsAppController {
      * @todo ...could possibly be just Pay.. and add Chained Payment receipients dynamically...
      */
     public function cpay() {
-	$amount = $this->request->data['Task']['amount'];
-	#debug($this->request->data);break;
-	if(is_numeric($amount)) {
-	    
-	    $amount = number_format ($amount, 2);
-	    
-	    App::uses('User', 'Users.Model');
-            $User = new User;
-	    $payeeInfo = $User->findById($this->request->data['Task']['assignee_id']);
-	    
-	    $success = $this->ConnectionPaypal->doChainedPayment(array(
-		'amount' => $amount,
-		'secondaryAccount' => $payeeInfo['User']['email'],
-		'returnUrl' => $this->referer(),
-		'cancelUrl' => $this->referer()
-	    ));
+		$amount = $this->request->data['Task']['amount'];
 
-	    if($success) {
-		if(ConnectionManager::getDataSource('paypal')->config['environment'] == 'sandbox') {
-		    $this->redirect('https://www.sandbox.paypal.com/webscr?cmd=_ap-payment&paykey='.$success->payKey);
-		} else {
-		    $this->redirect('https://www.paypal.com/webscr?cmd=_ap-payment&paykey='.$success->payKey);
-		}
-	    } else {
-		debug($success);break;
-	    }
-	    
-	}
+		if (is_numeric($amount)) {
+		    
+		    $amount = number_format ($amount, 2);
+		    
+		    App::uses('User', 'Users.Model');
+            $User = new User;
+		    $payeeInfo = $User->findById($this->request->data['Task']['assignee_id']);
+		    
+		    $success = $this->ConnectionPaypal->doChainedPayment(array(
+				'amount' => $amount,
+				'secondaryAccount' => $payeeInfo['User']['email'],
+				'returnUrl' => $this->referer(),
+				'cancelUrl' => $this->referer()
+		    ));
 	
-	$this->Session->setFlash('There was an error.');
-	$this->redirect($this->referer());
+		    if ($success) {
+				if (ConnectionManager::getDataSource('paypal')->config['environment'] == 'sandbox') {
+				    $this->redirect('https://www.sandbox.paypal.com/webscr?cmd=_ap-payment&paykey='.$success->payKey);
+				} else {
+				    $this->redirect('https://www.paypal.com/webscr?cmd=_ap-payment&paykey='.$success->payKey);
+				}
+		    } else {
+				debug($success);
+				break;
+		    }
+		    
+		}
+		
+		$this->Session->setFlash('There was an error.');
+		$this->redirect($this->referer());
     }
     
 }
